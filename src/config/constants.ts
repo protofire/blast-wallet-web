318ca08ef2d44b186ef008e0e984ec83419d1533
nick8319 <zhiltsov.nick@gmail.com>
2024-03-23 21:24:29 +0100

    fix: linter

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -3,10 +3,8 @@ import chains from './chains'
 export const IS_PRODUCTION = process.env.NEXT_PUBLIC_IS_PRODUCTION === 'true'
 export const IS_DEV = process.env.NODE_ENV === 'development'
 
-export const GATEWAY_URL_PRODUCTION =
-  process.env.NEXT_PUBLIC_GATEWAY_URL_PRODUCTION || 'https://gateway.blast-safe.io'
-export const GATEWAY_URL_STAGING =
-  process.env.NEXT_PUBLIC_GATEWAY_URL_STAGING || 'https://gateway.blast-safe.io'
+export const GATEWAY_URL_PRODUCTION = process.env.NEXT_PUBLIC_GATEWAY_URL_PRODUCTION || 'https://gateway.blast-safe.io'
+export const GATEWAY_URL_STAGING = process.env.NEXT_PUBLIC_GATEWAY_URL_STAGING || 'https://gateway.blast-safe.io'
 
 // Magic numbers
 export const POLLING_INTERVAL = 15_000
