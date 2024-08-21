bd9d417cb25db67f8b5a862d4e53f988e9428c7f
Nikita Zasimuk <nikita.zasimuk@protofire.io>
2024-08-21 16:40:46 +0300

    feat: update footer

--- src/components/common/Footer/index.tsx
+++ src/components/common/Footer/index.tsx
@@ -9,7 +9,7 @@ import packageJson from '../../../../package.json'
 //import AppstoreButton from '../AppStoreButton'
 import ExternalLink from '../ExternalLink'
 import MUILink from '@mui/material/Link'
-import { /* HELP_CENTER_URL,  */ IS_DEV, IS_OFFICIAL_HOST } from '@/config/constants'
+import { HELP_CENTER_URL /*, IS_DEV, IS_OFFICIAL_HOST*/ } from '@/config/constants'
 
 const footerPages = [
   AppRoutes.welcome.index,
@@ -46,15 +46,12 @@ const Footer = (): ReactElement | null => {
   return (
     <footer className={css.container}>
       <ul>
-        {IS_OFFICIAL_HOST || IS_DEV ? (
+        {false ? (
           <>
             <li>
-              <Typography variant="caption">&copy;2022–{new Date().getFullYear()} Blast</Typography>
+              <Typography variant="caption">&copy;2022–{new Date().getFullYear()} Core Contributors GmbH</Typography>
             </li>
             <li>
-              <FooterLink href={getHref(AppRoutes.blastGold)}>Blast Gold</FooterLink>
-            </li>
-            {/* <li>
               <FooterLink href={getHref(AppRoutes.terms)}>Terms</FooterLink>
             </li>
             <li>
@@ -68,18 +65,27 @@ const Footer = (): ReactElement | null => {
             </li>
             <li>
               <FooterLink href={getHref(AppRoutes.cookie)}>Cookie policy</FooterLink>
-            </li> */}
+            </li>
             <li>
               <FooterLink href={getHref(AppRoutes.settings.index)}>Preferences</FooterLink>
             </li>
-            {/* <li>
+            <li>
               <ExternalLink href={HELP_CENTER_URL} noIcon sx={{ span: { textDecoration: 'underline' } }}>
                 Help
               </ExternalLink>
-            </li> */}
+            </li>
           </>
         ) : null}
 
+        <li>
+          <Typography variant="caption">&copy;2022–{new Date().getFullYear()} Blast</Typography>
+        </li>
+        <li>
+          <FooterLink href={getHref(AppRoutes.blastGold)}>Blast Gold</FooterLink>
+        </li>
+        <li>
+          <FooterLink href={getHref(AppRoutes.settings.index)}>Preferences</FooterLink>
+        </li>
         <li>
           <ExternalLink href={`${packageJson.homepage}/releases/tag/v${packageJson.version}`} noIcon>
             <SvgIcon component={GitHubIcon} inheritViewBox fontSize="inherit" sx={{ mr: 0.5 }} /> v{packageJson.version}
