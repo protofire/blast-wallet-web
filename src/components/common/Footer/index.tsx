ef167c93e5772d736bf4459f0b4ba0190817a035
Den Smalonski <dzianis.smalonski@protofire.io>
2024-09-17 17:39:09 +0200

    feat: add help link

--- src/components/common/Footer/index.tsx
+++ src/components/common/Footer/index.tsx
@@ -75,7 +75,15 @@ const Footer = (): ReactElement | null => {
               </ExternalLink>
             </li>
           </>
-        ) : null}
+        ) : (
+          <>
+            <li>
+              <ExternalLink href={HELP_CENTER_URL} noIcon sx={{ span: { textDecoration: 'underline' } }}>
+                Help
+              </ExternalLink>
+            </li>
+          </>
+        )}
 
         <li>
           <Typography variant="caption">&copy;2022–{new Date().getFullYear()} Blast</Typography>
