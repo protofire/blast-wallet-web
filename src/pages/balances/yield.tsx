4068e7dcf41579eb8871948ed656d0a1c837db09
Den Smalonski <dzianis.smalonski@protofire.io>
2024-10-14 16:36:34 +0200

    feat: add notification about blast yield

--- src/pages/balances/yield.tsx
+++ src/pages/balances/yield.tsx
@@ -2,7 +2,8 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 
 import AssetsHeader from '@/components/balances/AssetsHeader'
-import YieldTable from '@/components/balances/YieldTable'
+import MuiLink from '@mui/material/Link'
+import Link from 'next/link'
 
 const Yield: NextPage = () => {
   return (
@@ -14,7 +15,14 @@ const Yield: NextPage = () => {
       <AssetsHeader />
 
       <main>
-        <YieldTable />
+        <div>
+          Yield control functionality is now moved into separate safe app. You can find it in the list of Safe apps or
+          by following this{' '}
+          <Link href="https://blast-safe.io/share/safe-app?appUrl=https%3A%2F%2Fyield.blast-safe.io" legacyBehavior>
+            <MuiLink>link</MuiLink>
+          </Link>
+          .
+        </div>
       </main>
     </>
   )
